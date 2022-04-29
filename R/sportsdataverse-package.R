15e48edc5b5e1b4ff9a7d7d7593719ab11ad63aa
saiemgilani0 <saiem.gilani@gmail.com>
2022-04-29 02:27:21 -0400

    update namespace for imports from each of the core packages

--- R/sportsdataverse-package.R
+++ R/sportsdataverse-package.R
@@ -1,6 +1,16 @@
 #' @keywords internal
 "_PACKAGE"
 
+# Suppress R CMD check note
+#' @importFrom baseballr most_recent_mlb_season
+#' @importFrom cfbfastR espn_cfb_calendar
+#' @importFrom chessR  chessdotcom_leaderboard
+#' @importFrom fastRhockey most_recent_nhl_season
+#' @importFrom hoopR login
+#' @importFrom toRvik current_season
+#' @importFrom wehoop most_recent_wnba_season
+NULL
+
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
