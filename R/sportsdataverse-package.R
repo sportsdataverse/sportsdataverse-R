5f49d987cb3b39c167833eb53c2a936bd7c8faff
saiemgilani0 <saiem.gilani@gmail.com>
2022-04-29 02:41:04 -0400

    update namespace

--- R/sportsdataverse-package.R
+++ R/sportsdataverse-package.R
@@ -3,7 +3,7 @@
 
 # Suppress R CMD check note
 #' @importFrom baseballr most_recent_mlb_season
-#' @importFrom cfbfastR espn_cfb_calendar
+#' @importFrom cfbfastR load_cfb_pbp
 #' @importFrom chessR  chessdotcom_leaderboard
 #' @importFrom fastRhockey most_recent_nhl_season
 #' @importFrom hoopR login
