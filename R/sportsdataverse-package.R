eee8e1928a66935d8dedd28b08cad627a521f731
saiemgilani <saiem.gilani@gmail.com>
2023-01-05 08:54:02 -0500

    update chessR

--- R/sportsdataverse-package.R
+++ R/sportsdataverse-package.R
@@ -4,7 +4,6 @@
 # Suppress R CMD check note
 #' @importFrom baseballr most_recent_mlb_season
 #' @importFrom cfbfastR load_cfb_pbp
-#' @importFrom chessR  chessdotcom_leaderboard
 #' @importFrom fastRhockey most_recent_nhl_season
 #' @importFrom hoopR login
 #' @importFrom wehoop load_wbb_pbp
